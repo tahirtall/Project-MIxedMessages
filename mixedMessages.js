755cb1f8b878d29c3332253e89c315faa7538029
tahirtall <tahirtall46@gmail.com>
2021-09-07 14:43:20 -0500

    added a driver program, finished working on the function where a a random quote was being returned

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,3 +1,36 @@
+const mixedMessagesArray = [
+    "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart - Helen Keller",
+    "Keep your face always toward the sunshine - and shadows will fall behind you. - Walt Whitman",
+    "The best preparation for tomorrow is doing your best today. - H. Jackson Brown, Jr.",
+    "Perfection is not attainable, but if we chase perfection we can catch excellence. - Vince Lombardi",
+    "Even if I knew that tomorrow the world would go to pieces, I would still plant my apple tree. - Martin Luther",
+    "Your present circumstances don't determine where you can go; they merely determine where you start. - Nido Qubein",
+    "Happiness resides not in possessions, and not in gold, happiness dwells in the soul. - Democritus",
+    "Thinking: the talking of the soul with itself. - Plato",
+    "I believe every human has a finite number of heartbeats. I don't intend to waste any of mine. - Neil Armstrong",
+    "The best way out is always through. - Robert Frost"
+]
+
+//Driver program
+const driverApp = () => {
+
+}
+
+// Generates a random quote from "mixedMessagesArray"
+const mixedMessages = () => {
+    // Generates a random number
+    const randomNumber = Math.floor(Math.random() * mixedMessagesArray.length);
+    return mixedMessagesArray[randomNumber];
+}
+
+// adds a new quote to the "mixedMessagesArray"
+const addNewMixedMessage = quote => {
+    
+}
+
+mixedMessagesDriver();
+
+
 /*
     - The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart - Helen Keller
     - Keep your face always toward the sunshine - and shadows will fall behind you. - Walt Whitman
